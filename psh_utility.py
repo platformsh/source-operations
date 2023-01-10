7b4e3601bd7e0f4973476b9b8a5f42816f8ba028
Paul Gilzow <paul.gilzow@platform.sh>
2023-01-10 16:04:14 -0600

    update tool version in anticipation of PR acceptance

--- psh_utility.py
+++ psh_utility.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 from psh_logging import outputError
 
-SOURCE_OP_TOOLS_VERSION = '0.2.5'
+SOURCE_OP_TOOLS_VERSION = '0.2.6'
 PSH_COMMON_MESSAGES = {
     'psh_cli': {
         'event': 'Checking for the Platform.sh CLI tool',
